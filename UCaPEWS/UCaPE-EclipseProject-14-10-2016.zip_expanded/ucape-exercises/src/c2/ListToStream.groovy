33b62108ac68b92f8d6bea01937867c7a79d2c27
Conner Weatherston <40167111@live.napier.ac.uk>
2017-01-10 11:28:40 +0000

    Almost finished exercise 2.
    Need to update lab book for week 2 and all the pdfs for the module into
    repo.

---
+++ UCaPEWS/UCaPE-EclipseProject-14-10-2016.zip_expanded/ucape-exercises/src/c2/ListToStream.groovy
@@ -0,0 +1,21 @@
+package c2
+
+import org.jcsp.lang.*
+
+class ListToStream implements CSProcess{
+	
+	def ChannelInput inChannel
+	def ChannelOutput outChannel
+	
+	void run (){
+		def inList = inChannel.read()
+		while (inList[0] != -1)
+		{
+			// hint: output	list elements as single integers
+				for ( i in 0 ..< inList.size)
+					outChannel.write(inList[i])
+				inList = inChannel.read()
+		}
+		outChannel.write(-1)
+	}
+}
